1873a17e3619404ed6345a0e8a3c31f847a16459
AnythingFree <katarina.despotovic@student.pmf.unibl.org>
2023-07-22 15:03:30 +0200

    par komenatara dodano

--- ProjekatSimulacijaOS/src/cpu/CPU.java
+++ ProjekatSimulacijaOS/src/cpu/CPU.java
@@ -1,8 +1,11 @@
 package cpu;
 
+import java.util.ArrayList;
+
+import interfaces.CPU_i;
 import simulacije_jer_ih_jos_nemamo.Simulacija_RAMmemorije;
 
-public class CPU{
+public class CPU implements CPU_i{
 	
 	// hard codovali smo stek memoriju ali moze i dinamicki da se mijenja
 	int stack_pointer = 0;
@@ -10,7 +13,7 @@ public class CPU{
 	
 	int instruction_pointer = 0;
 	
-	
+	boolean zero, carry;
 	
 	
 	private void push(int a) {
@@ -20,6 +23,66 @@ public class CPU{
 	private int pop() {
 		int a = Simulacija_RAMmemorije.niz[--stack_pointer];
 		return a;
+	}
+
+	@Override
+	public void execute_code(ArrayList<Integer> kodovi_binarni) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void execute_instruction(int naredba, int a, int b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hlt() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void add(int a, int b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sub(int a, int b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mul(int a, int b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void div(int a, int b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dec(int a) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void inc(int a) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void increment_instr_point() {
+		// cita trenutnu naredbu, zna koliko mjesta zauzima zajendo sa argumentima, povecava za toliko mijesta
+		// u ovom slucaju, samo pop nema argumente svi ostali imaju 1 argument
 	}
 
 
