4cdda25ea5fc5cf6e338f3a846d8e1c3f1303e8a
AnythingFree <katarina.despotovic@student.pmf.unibl.org>
2023-07-25 18:33:27 +0200

    Popravljam greske oko brisanja bin fajla.

--- ProjekatSimulacijaOS/src/cpu/CPU.java
+++ ProjekatSimulacijaOS/src/cpu/CPU.java
@@ -3,80 +3,79 @@ package cpu;
 import java.util.ArrayList;
 
 import interfaces.CPU_i;
-import memory_management.RAM;
 
 public class CPU implements CPU_i{
-	
+
 	// hard codovali smo stek memoriju ali moze i dinamicki da se mijenja
 	int stack_pointer = 0;
 	int end_of_stack = 9;
-	
+
 	int instruction_pointer = 0;
-	
+
 	boolean zero, carry;
-	
-	
+
+
 	private void push(int a) {
-		RAM.niz[stack_pointer++] = a;
+		//RAM.niz[stack_pointer++] = a;
 	}
 
 	private int pop() {
-		int a = RAM.niz[--stack_pointer];
+		//int a = RAM.niz[--stack_pointer];
 		return a;
 	}
 
 	@Override
 	public void execute_code(ArrayList<Integer> kodovi_binarni) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void execute_instruction(int naredba, int a, int b) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void hlt() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void add(int a, int b) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void sub(int a, int b) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mul(int a, int b) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void div(int a, int b) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void dec(int a) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void inc(int a) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
