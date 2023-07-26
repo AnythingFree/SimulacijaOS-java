e540c0a66d00666946f2a5b83e3236e497c5099a
AnythingFree <katarina.despotovic@student.pmf.unibl.org>
2023-07-26 18:33:27 +0200

    Ociscen kod u Asembler_interpreter klasi i prebacena funkcija write_file u Asembler klasu i prepravljena da ispisuje u jednom redu u datoteku, vidjecemo jel to dobro.

--- ProjekatSimulacijaOS/src/assembler/Asembler.java
+++ ProjekatSimulacijaOS/src/assembler/Asembler.java
@@ -1,20 +1,45 @@
 package assembler;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
 import cpu.CPU;
 
 public class Asembler {
 	private Asembler_interpreter a1 = new Asembler_interpreter();
 	private CPU a2 = new CPU();
-	
-	public void create_machine_code(String path_to_file, String new_file_name) {
-		a1.asemble(path_to_file, new_file_name);
-		// write that code to txt file
-		
+
+	protected void create_machine_code(String path_to_file, String OUTPUT_FILE_PATH) {
+
+		// pretvara u masinski kod binarno
+		ArrayList<String> binaryCode = a1.asemble(path_to_file);
+
+		// pise u fajl
+		try {
+			writeBinaryCodeToFile(binaryCode, OUTPUT_FILE_PATH);
+			System.out.println("Asembliranje uspješno završeno. Binarni kod je sačuvan u " + OUTPUT_FILE_PATH);
+		} catch (IOException e) {
+			System.err.println("Greška prilikom obrade datoteka: " + e.getMessage());
+		}
+
 	}
 
-	public void execute_code(String path_to_file) {
+	protected void execute_code(String path_to_file) {
 		// read machine code
 		// execute code on cpu
 	}
-	
+
+	// ====================================
+
+	// upisuje binarni kod u fajl .txt
+	private void writeBinaryCodeToFile(ArrayList<String> binaryCode, String OUTPUT_FILE_PATH) throws IOException {
+		BufferedWriter writer = new BufferedWriter(new FileWriter(OUTPUT_FILE_PATH));
+		for (String piece : binaryCode) {
+			writer.write(piece + " ");
+		}
+		writer.close();
+	}
+
 }
--- ProjekatSimulacijaOS/src/assembler/Test.java
+++ ProjekatSimulacijaOS/src/assembler/Test.java
@@ -5,9 +5,9 @@ public class Test {
 	public static void main(String[] args) {
 		Asembler a = new Asembler();
 		String s = "program.asm";
-		a.create_machine_code(s, "masinski_kod_program"); // ocekuje se da napravi .txt fajl sa masinskim kodom
-		//a.execute_code(s);
-		
+		a.create_machine_code(s, "masinski_kod_program");
+		// a.execute_code(s);
+
 	}
 
 }
