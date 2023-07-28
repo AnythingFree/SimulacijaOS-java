b984b2df742f993cf23d8cd0750d8014148f0adc
AnythingFree <katarina.despotovic@student.pmf.unibl.org>
2023-07-28 16:03:10 +0200

    Namjestila javafx na lokalnom komp i rijesila probleme oko toga da mogu da pokrenem GUI klasu.

--- ProjekatSimulacijaOS/src/shell/GUI.java
+++ ProjekatSimulacijaOS/src/shell/GUI.java
@@ -1,88 +1,82 @@
 package shell;
-import java.io.IOException;
+
 import java.io.OutputStream;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
+import java.util.Scanner;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.stage.Stage;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.VBox;
 
 public class GUI extends Application {
 	private static String textToShow;
-	private static TextArea top=new TextArea();
-	private static TextField bottom=new TextField();
+	private static TextArea top = new TextArea();
+	private static TextField bottom = new TextField();
 	private static Button light;
 	private static Button dark;
-	private PipedInputStream input=new PipedInputStream();
-	private PipedOutputStream output=new PipedOutputStream();
-	private StringBuilder outSB=new StringBuilder();
+	private PipedInputStream input = new PipedInputStream();
+	private PipedOutputStream output = new PipedOutputStream();
+	private StringBuilder outSB = new StringBuilder();
 	private OutputStream outputStream;
-	private int length1=0;
-	
-	
-	
+	private int length1 = 0;
+
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-	    public static void main(String[] args) {
-	        System.out.println("Dobrodošli u CLI! Unesite naredbu ili 'exit' za izlaz.");
+	}
 
-	        Scanner scanner = new Scanner(System.in);
-	        String command;
+	public static void main(String[] args) {
+		System.out.println("Dobrodošli u CLI! Unesite naredbu ili 'exit' za izlaz.");
 
-	        do {
-	            System.out.print("> ");
-	            command = scanner.nextLine();
-	            processCommand(command);
-	        } while (!command.equalsIgnoreCase("exit"));
+		Scanner scanner = new Scanner(System.in);
+		String command;
 
-	        scanner.close();
-	    }
+		do {
+			System.out.print("> ");
+			command = scanner.nextLine();
+			processCommand(command);
+		} while (!command.equalsIgnoreCase("exit"));
 
-	    public static void processCommand(String command) {
-	        switch (command) {
-	            case "cd":
-	                System.out.println("Izvršena naredba: prelazi u ciljni direktorijum");
-	                // Implementirajte logiku za prelazak u ciljni direktorijum
-	                break;
-	            case "dir":
-	            case "ls":
-	                System.out.println("Izvršena naredba: lista trenutni direktorijum");
-	                // Implementirajte logiku za ispis trenutnog direktorijuma
-	                break;
-	            case "ps":
-	                System.out.println("Izvršena naredba: lista procese");
-	                // Implementirajte logiku za ispis procesa i osnovnih informacija o njima
-	                break;
-	            case "mkdir":
-	                System.out.println("Izvršena naredba: pravi direktorijum");
-	                // Implementirajte logiku za stvaranje direktorija
-	                break;
-	            case "run":
-	                System.out.println("Izvršena naredba: pokreće proces");
-	                // Implementirajte logiku za pokretanje procesa
-	                break;
-	            case "mem":
-	                System.out.println("Izvršena naredba: zauzeće RAM memorije");
-	                // Implementirajte logiku za ispis zauzeća RAM memorije
-	                break;
-	            case "exit":
-	                System.out.println("Izvršena naredba: gasi OS");
-	                // Implementirajte logiku za izlazak iz CLI
-	                break;
-	            default:
-	                System.out.println("Nepoznata naredba: " + command);
-	                break;
-	        }
-	    }
-		
+		scanner.close();
+	}
+
+	public static void processCommand(String command) {
+		switch (command) {
+		case "cd":
+			System.out.println("Izvršena naredba: prelazi u ciljni direktorijum");
+			// Implementirajte logiku za prelazak u ciljni direktorijum
+			break;
+		case "dir":
+		case "ls":
+			System.out.println("Izvršena naredba: lista trenutni direktorijum");
+			// Implementirajte logiku za ispis trenutnog direktorijuma
+			break;
+		case "ps":
+			System.out.println("Izvršena naredba: lista procese");
+			// Implementirajte logiku za ispis procesa i osnovnih informacija o njima
+			break;
+		case "mkdir":
+			System.out.println("Izvršena naredba: pravi direktorijum");
+			// Implementirajte logiku za stvaranje direktorija
+			break;
+		case "run":
+			System.out.println("Izvršena naredba: pokreće proces");
+			// Implementirajte logiku za pokretanje procesa
+			break;
+		case "mem":
+			System.out.println("Izvršena naredba: zauzeće RAM memorije");
+			// Implementirajte logiku za ispis zauzeća RAM memorije
+			break;
+		case "exit":
+			System.out.println("Izvršena naredba: gasi OS");
+			// Implementirajte logiku za izlazak iz CLI
+			break;
+		default:
+			System.out.println("Nepoznata naredba: " + command);
+			break;
+		}
+	}
+
 }
-	
